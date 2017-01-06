ae484dc05fd683594b02b721fa4b86dea9d1faad
Linh&ShawnD <linhshawn-d@LinhShawnDs-MacBook-Pro.local>
2017-01-06 15:33:37 -0800

    First Time Commit

---
+++ Digital Clock by LD/Clock.swift
@@ -0,0 +1,19 @@
+//
+//  Clock.swift
+//  Digital Clock by LD
+//
+//  Created by Linh&ShawnD on 2017-01-03.
+//  Copyright © 2017 Shali Designs. All rights reserved.
+//
+
+import Foundation
+
+class Clock {
+    
+    var currentTime: NSDate {
+        return NSDate()
+
+    }
+    
+    
+}
--- Digital Clock by LD/ViewController.swift
+++ Digital Clock by LD/ViewController.swift
@@ -10,16 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+   let clock = Clock()
+    
+    @IBOutlet weak var timelabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewDidAppear(_ animated: Bool) {
+    super.viewDidAppear(animated)
+        
+        let formatter = DateFormatter()
+        formatter.timeStyle = .medium
+        timelabel.text = "\(formatter.string(from: clock.currentTime as Date))"
     }
-
-
 }
 
